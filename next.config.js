abdd4fcedbcbc2b08a991b4e798d6fabfc64606c
Tommy Otegui <tommyotegui@placeholder.local>
2026-04-21 23:20:44 -0300

    Add /crm rewrite to duo-insight-desk deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/crm',
+        destination: 'https://duo-insight-desk.vercel.app/crm',
+      },
+      {
+        source: '/crm/:path*',
+        destination: 'https://duo-insight-desk.vercel.app/crm/:path*',
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
